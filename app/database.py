e3459bc22d0dae63ffb5913251091113bcbc96c4
TomPouv <contact@thomas-pouvreau.fr>
2024-02-19 16:48:25 +0100

    create base skeleton using brand new sqlmodel

---
+++ app/database.py
@@ -0,0 +1,13 @@
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+import models
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from database import create_db_and_tables 
 
 app = FastAPI()
 
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get("/")
 async def root():
