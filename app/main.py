747c55faa811c687ad875effd179a8a9525ac164
TomPouv <contact@thomas-pouvreau.fr>
2024-02-19 16:52:33 +0100

    add the three necessary endpoints

--- app/main.py
+++ app/main.py
@@ -9,6 +9,14 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/register")
+async def register( username, password):
+    return {"username": username, "password": password}
+
+@app.post("/signin")
+async def signin( username, password):
+    return {"username": username, "password": password}
+
+@app.delete("/user")
+async def delete_user():
+    print(" current_user needs to be retrevied and deleted") 
