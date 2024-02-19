c957758ef75307d599ed57753c323b9d28bdcd64
TomPouv <contact@thomas-pouvreau.fr>
2024-02-19 17:38:13 +0100

    trying to implement the code from fastapi documentation

--- app/main.py
+++ app/main.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI
 from database import create_db_and_tables, engine
 from sqlmodel import Session, select
 from models import User
+from passlib.context import CryptContext
 
 app = FastAPI()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(plain_password):
+    return pwd_context.hash(plain_password)
 
 
 @app.on_event("startup")
@@ -13,11 +20,13 @@ def on_startup():
 
 @app.post("/register")
 async def register( user: User):
+    hashed_password = get_password_hash(user.password_hash)
+    user.password_hash = hashed_password
     with Session(engine) as session:
         session.add(user)
         session.commit()
         session.refresh(user)
-        return user 
+        return user
 
 
 @app.post("/signin")
