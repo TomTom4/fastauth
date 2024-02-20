149dc56a85f8fa35ade40addd3b307276af89b59
TomPouv <contact@thomas-pouvreau.fr>
2024-02-20 17:59:26 +0100

    implement simple password flow based on fastapi documentation

--- app/main.py
+++ app/main.py
@@ -1,15 +1,43 @@
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from fastapi import Depends, FastAPI, HTTPException, status
 from database import create_db_and_tables, engine
 from sqlmodel import Session, select
+from pydantic import BaseModel
 from models import User
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 app = FastAPI()
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="signin")
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -17,10 +45,34 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(plain_password):
     return pwd_context.hash(plain_password)
 
+def decode_token(token):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    username: str = payload.get("sub")
+    if username is None:
+        raise credentials_exception
+    return TokenData(username=username)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        token_data = decode_token(token)
+    except JWTError:
+        raise credentials_exception
+    with Session(engine) as session:
+        statement = select(User).where(User.username == token_data.username)
+        user = session.exec(statement).first()
+        if not user:
+            raise credentials_exception
+        return user
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
 
 @app.post("/register")
 async def register( user: User):
@@ -34,13 +86,18 @@ async def register( user: User):
 
 
 @app.post("/signin")
-async def signin( user: User):
+async def signin( form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     with Session(engine) as session:
-        statement = select(User).where(User.username == user.username)
-        db_user = session.exec(statement).first()
-        if verify_password(user.password_hash, db_user.password_hash):
-            return user
+        statement = select(User).where(User.username == form_data.username)
+        user = session.exec(statement).first()
+        if verify_password(form_data.password,user.password_hash):
+            access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+            access_token = create_access_token(data={"sub": user.username},
+                                                 expires_delta=access_token_expires)
+            return Token(access_token=access_token, token_type="bearer")
+
 
 @app.delete("/user")
-async def delete_user(token: Annotated[str, Depends(oauth2_scheme)]):
+async def delete_user(current_user: Annotated[User, Depends(get_current_user)]):
     print(" current_user needs to be retrevied and deleted") 
+    return current_user
