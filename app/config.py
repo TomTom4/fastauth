bbc17e5ae475653593e2e0f4cca5d02c42530805
TomPouv <contact@thomas-pouvreau.fr>
2024-02-20 18:32:08 +0100

    use .env for configurations

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
-    secret_key : str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-    algorithm : str = "HS256" 
+    secret_key : str = "" 
+    algorithm : str =  "" 
     access_token_expires_minutes : int = 30
+
+    model_config = SettingsConfigDict(env_file='.env')
