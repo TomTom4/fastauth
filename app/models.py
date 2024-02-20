36bad5df020f60000fbbd72c89f0b49db5315e80
TomPouv <contact@thomas-pouvreau.fr>
2024-02-20 15:39:18 +0100

    add uniqueness constraint and email formatted string to User Data Model ensuring username are always email adresses

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from typing import Optional
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, AutoString
+from pydantic import EmailStr
 
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    username: str
+    username: EmailStr  = Field(unique=True, index=True, sa_type=AutoString)
     password_hash: str
