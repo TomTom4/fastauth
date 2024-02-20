89dcb70ad85b81a96ac1a5aaeaa9d38a273cc06e
TomPouv <contact@thomas-pouvreau.fr>
2024-02-20 18:42:04 +0100

    put database_url into configurations, set it from .env

--- app/configurations.py
+++ app/configurations.py
@@ -5,5 +5,6 @@ class Settings(BaseSettings):
     secret_key : str = "" 
     algorithm : str =  "" 
     access_token_expires_minutes : int = 30
+    database_url : str = ""
 
     model_config = SettingsConfigDict(env_file='.env')
--- app/database.py
+++ app/database.py
@@ -1,11 +1,12 @@
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 import models
+from configurations import Settings
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+settings = Settings()
+
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+engine = create_engine(settings.database_url, echo=True, connect_args=connect_args)
 
 
 def create_db_and_tables():
