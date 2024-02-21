a0a15a498036e25240337178fff53412b5a7ca1b
TomPouv <contact@thomas-pouvreau.fr>
2024-02-21 21:15:20 +0100

    update test with app import, make pytest work, make the test pass

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,8 @@
-from app.main import app 
 from fastapi.testclient import TestClient
+from src.main import app 
 
 test_client = TestClient(app)
 
 def test_main():
-    response = client.delete('/user')
+    response = test_client.delete('/user')
     assert response.status_code == 401
