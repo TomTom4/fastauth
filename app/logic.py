362222b3414c94f162aad9df685c51642eae82e1
TomPouv <contact@thomas-pouvreau.fr>
2024-02-21 16:31:22 +0100

    extract logic into a logic module - needs to be renamed though

---
+++ app/logic.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta, timezone
+from configurations import Settings
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+
+settings = Settings()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key,
+                             algorithm=settings.algorithm)
+    return encoded_jwt
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(plain_password):
+    return pwd_context.hash(plain_password)
+
+def decode_token(token):
+    payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+    username: str = payload.get("sub")
+    if username is None:
+        raise credentials_exception
+    return TokenData(username=username)
