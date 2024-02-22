73cb45e709ad604b1c14d55ffcf6727cdc30f0db
TomPouv <contact@thomas-pouvreau.fr>
2024-02-22 17:23:52 +0100

    turn session into a dependency trough get_session

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,12 +1,25 @@
 from typing import Annotated
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import Session, select
+from jose import JWTError
+from src.database import engine
+from src.models import User
 from src.logic import decode_token
+
+
+async def get_session():
+    with Session(engine) as session:
+        yield session
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="signin")
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    session: Annotated[Session, Depends(get_session)],
+):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -16,9 +29,8 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         token_data = decode_token(token)
     except JWTError:
         raise credentials_exception
-    with Session(engine) as session:
-        statement = select(User).where(User.username == token_data.username)
-        user = session.exec(statement).first()
-        if not user:
-            raise credentials_exception
-        return user
+    statement = select(User).where(User.username == token_data.username)
+    user = session.exec(statement).first()
+    if not user:
+        raise credentials_exception
+    return user
