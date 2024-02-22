145b1ccfaae2f8c034a61d37b6f15d59aa49bf85
TomPouv <contact@thomas-pouvreau.fr>
2024-02-22 17:34:17 +0100

    fix bug introduced during code cleaning

--- src/logic.py
+++ src/logic.py
@@ -1,11 +1,14 @@
 from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
-from jose import JWTError, jwt
+from jose import jwt
+from fastapi import HTTPException, status
 from src.configurations import Settings
+from src.schemas import TokenData
 
-settings = Settings()
+settings: Settings = Settings()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
@@ -14,17 +17,26 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.secret_key,
-                             algorithm=settings.algorithm)
+    encoded_jwt = jwt.encode(
+        to_encode, settings.secret_key, algorithm=settings.algorithm
+    )
     return encoded_jwt
+
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+
 def get_password_hash(plain_password):
     return pwd_context.hash(plain_password)
 
+
 def decode_token(token):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
     username: str = payload.get("sub")
     if username is None:
