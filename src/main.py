76cff3fef16ed90df1ef15bc2f9d73297731038d
TomPouv <contact@thomas-pouvreau.fr>
2024-02-22 17:27:12 +0100

    update code using get_session

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ from fastapi import Depends, FastAPI
 from sqlmodel import Session, select
 from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
-from src.database import create_db_and_tables, engine
-from src.dependencies import get_current_user
+from src.database import create_db_and_tables
+from src.dependencies import get_session, get_current_user
 from src.models import User
 from src.schemas import Token
 from src.logic import create_access_token, verify_password, get_password_hash
@@ -24,29 +24,28 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/register")
-async def register(user: User):
+async def register(user: User, session: Annotated[Session, Depends(get_session)]):
     hashed_password = get_password_hash(user.password_hash)
     user.password_hash = hashed_password
-    with Session(engine) as session:
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-        return user
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
 
 
 @app.post("/signin")
-async def signin(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    with Session(engine) as session:
-        statement = select(User).where(User.username == form_data.username)
-        user: User = session.exec(statement).first()
-        if verify_password(form_data.password, user.password_hash):
-            access_token_expires = timedelta(
-                minutes=settings.access_token_expires_minutes
-            )
-            access_token = create_access_token(
-                data={"sub": user.username}, expires_delta=access_token_expires
-            )
-            return Token(access_token=access_token, token_type="bearer")
+async def signin(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: Annotated[Session, Depends(get_session)],
+):
+    statement = select(User).where(User.username == form_data.username)
+    user: User = session.exec(statement).first()
+    if verify_password(form_data.password, user.password_hash):
+        access_token_expires = timedelta(minutes=settings.access_token_expires_minutes)
+        access_token = create_access_token(
+            data={"sub": user.username}, expires_delta=access_token_expires
+        )
+        return Token(access_token=access_token, token_type="bearer")
 
 
 @app.delete("/user")
