ea0f13fe93fb2736ff3beb6118b71056abe64e81
TomPouv <contact@thomas-pouvreau.fr>
2024-02-23 14:43:56 +0100

    minimum tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -32,17 +32,25 @@ def client_fixture(session: Session):
 
 
 def test_register(client: TestClient):
-    username = "mail@example.com"
-    response = client.post(
-        "/register", json={"username": username, "password_hash": "test1"}
-    )
+    user_as_dict = {"username": "mail@example.com", "password_hash": "test1"}
+    response = client.post("/register", json=user_as_dict)
     assert response.status_code == 200
     data = response.json()
-    assert data["username"] == username
+    assert data["username"] == user_as_dict["username"]
 
 
 def test_signin(client: TestClient):
-    pass
+    user_as_dict = {"username": "mail@example.com", "password_hash": "test1"}
+    client.post("/register", json=user_as_dict)
+    response = client.post(
+        "/signin",
+        data={
+            "username": user_as_dict["username"],
+            "password": user_as_dict["password_hash"],
+        },
+    )
+    assert response.status_code == 200
+    print(response.json())
 
 
 def test_delete_user(client: TestClient):
