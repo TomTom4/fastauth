e331659f658349582b99f3eaf8f57ad07eb8bb62
TomPouv <contact@thomas-pouvreau.fr>
2024-02-24 11:22:48 +0100

    cleaned test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,34 +1,4 @@
-import pytest
 from fastapi.testclient import TestClient
-from src.main import app
-from src.dependencies import get_session
-from sqlmodel import Session, create_engine, SQLModel
-from sqlmodel.pool import StaticPool
-
-
-@pytest.fixture(name="session")
-def session_fixture():
-    engine = create_engine(
-        "sqlite://",
-        connect_args={"check_same_thread": False},
-        poolclass=StaticPool,
-    )
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
-
-
-@pytest.fixture(name="client")
-def client_fixture(session: Session):
-
-    def get_session_override():
-        return session
-
-    app.dependency_overrides[get_session] = get_session_override
-    with TestClient(app) as client:
-        yield client
-
-    app.dependency_overrides.clear()
 
 
 def test_register(client: TestClient):
