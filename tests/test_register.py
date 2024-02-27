6ae5808c006424db255f9f45f6ff449475e84ac7
TomPouv <contact@thomas-pouvreau.fr>
2024-02-27 16:59:09 +0100

    strengthen test_register

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,14 +1,17 @@
 from fastapi.testclient import TestClient
-from sqlmodel import Session
+from sqlmodel import Session, select
 from src.models import User
 
 
-def test_register_new_user(client: TestClient):
+def test_register_new_user(client: TestClient, session: Session):
     user_as_dict = {"username": "mail@example.com", "password_hash": "test1"}
     response = client.post("/register", json=user_as_dict)
     assert response.status_code == 200
     data = response.json()
     assert data["username"] == user_as_dict["username"]
+    statement = select(User).where(User.username == user_as_dict["username"])
+    user = session.exec(statement).first()
+    assert user
 
 
 def test_register_existing_user(client: TestClient, session: Session):
