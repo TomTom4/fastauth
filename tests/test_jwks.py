e084365b6b729a78907996a432eb8e91cd2e5613
TomPouv <contact@thomas-pouvreau.fr>
2024-03-07 17:03:29 +0100

    passing asymetric  key test

--- tests/test_jwks.py
+++ tests/test_jwks.py
@@ -1,7 +1,7 @@
-from src.jwks import JWK, JWKS
+from src.jwks import ElipticCurveJWK
 
 
-def test_public_jwk_symetric_algorithm():
+def test_public_jwk_asymetric_algorithm():
     example_key = {
         "kty": "EC",
         "crv": "P-256",
@@ -10,16 +10,21 @@ def test_public_jwk_symetric_algorithm():
         "use": "enc",
         "kid": "1",
     }
-    # TODO: implement test
-    symetric_jwk = None
-    assert symetric_jwk == example_key
+    asymetric_jwk = ElipticCurveJWK(
+        crv="P-256",
+        x="MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
+        y="4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
+        use="enc",
+        kid="1",
+    )
+    assert asymetric_jwk.model_dump(exclude_none=True) == example_key
 
 
-def test_public_jwk_asymetric_algorithm():
+def test_public_jwk_symetric_algorithm():
     example_key = {}
-    asymetric_jwk = None
+    symetric_jwk = None
     # TODO: implement test
-    assert asymetric_jwk == example_key
+    assert symetric_jwk == example_key
 
 
 def test_public_jwks():
