9ce88b7cb27d9fdbfb7626d3d3432ba7a7591ed5
TomPouv <contact@thomas-pouvreau.fr>
2024-03-07 17:15:37 +0100

    create test for jwks

--- tests/test_jwks.py
+++ tests/test_jwks.py
@@ -1,4 +1,4 @@
-from src.jwks import ElipticCurveJWK, RSAJWK
+from src.jwks import ElipticCurveJWK, RSAJWK, JWKS
 
 
 def test_public_jwk_asymetric_algorithm():
@@ -48,5 +48,49 @@ def test_public_jwk_symetric_algorithm():
 
 
 def test_public_jwks():
-    # TODO: implement test
-    assert "jwks" == []
+    example_set = {
+        "keys": [
+            {
+                "kty": "EC",
+                "crv": "P-256",
+                "x": "MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
+                "y": "4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
+                "use": "enc",
+                "kid": "1",
+            },
+            {
+                "kty": "RSA",
+                "n": "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx\
+     4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMs\
+     tn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2\
+     QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbI\
+     SD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqb\
+     w0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
+                "e": "AQAB",
+                "alg": "RS256",
+                "kid": "2011-04-29",
+            },
+        ]
+    }
+    asymetric_jwk = ElipticCurveJWK(
+        crv="P-256",
+        x="MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
+        y="4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
+        use="enc",
+        kid="1",
+    )
+    symetric_jwk = RSAJWK(
+        n="0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx\
+     4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMs\
+     tn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2\
+     QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbI\
+     SD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqb\
+     w0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
+        e="AQAB",
+        alg="RS256",
+        kid="2011-04-29",
+    )
+
+    jwk_set = JWKS(keys=[asymetric_jwk, symetric_jwk])
+
+    assert jwk_set.model_dump(exclude_none=True) == example_set
