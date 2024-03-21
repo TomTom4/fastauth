10079bc819d250272bda4a34c6b54bad9da45a68
TomPouv <contact@thomas-pouvreau.fr>
2024-03-21 17:28:40 +0100

    test that token verification do work

---
+++ tests/test_token_verification.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+from jose import jwt
+
+
+def test_token_verification(client: TestClient):
+    print("hello")
+    user_as_dict = {"username": "mail@example.com", "password_hash": "test1"}
+    client.post("/register", json=user_as_dict)
+    response = client.post(
+        "/signin",
+        data={
+            "username": user_as_dict["username"],
+            "password": user_as_dict["password_hash"],
+        },
+    )
+    token = response.json()["access_token"]
+    assert response.status_code == 200
+    response = client.get("/.well-known/jwks.json")
+    assert response.status_code == 200
+    key = response.json()
+    algorithm = jwt.get_unverified_header(token).get("alg")
+    user_info = jwt.decode(token=token, key=key, algorithms=algorithm)
+    assert user_info["sub"] == user_as_dict["username"]
